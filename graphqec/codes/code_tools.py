0a0ded6101f9da9e4afbad18dc0451d094cccc03
EnTangledUpInBlue <ren.pankovich@gmail.com>
2025-07-30 13:06:27 -0400

    cleanup of unused functions from code_tools

--- graphqec/codes/code_tools.py
+++ graphqec/codes/code_tools.py
@@ -16,11 +16,9 @@ __all__ = [
     "add_rows",
     "binary_rank",
     "commutation_test",
-    "compare_rows",
     "compute_kernel",
     "prep_matrix",
     "row_extended_check",
-    "row_sort",
     "transpose_check",
 ]
 
@@ -147,68 +145,3 @@ def row_extended_check(check_matrix: list[list[int]]) -> list[list[int]]:
         ch_ext.append([0] * ii + [1] + [0] * (ncols - ii - 1))
 
     return check_matrix + ch_ext
-
-
-def row_sort(check_matrix: list[list[int]]) -> list[list[int]]:
-    r"""
-    Function taking in a check matrix as a list of binary lists and outputs
-    a sorted list of binary lists with the ordering determined by the
-    compare_rows function.
-    """
-
-    num = len(check_matrix)
-    if num < 2:
-        return check_matrix
-    else:
-        first_half = row_sort(check_matrix[: int(num / 2)])
-        second_half = row_sort(check_matrix[int(num / 2) :])
-        sorted_full = []
-
-        while len(first_half) > 0 and len(second_half) > 0:
-            comp = compare_rows(first_half[0], second_half[0])
-            if comp:
-                sorted_full.append(second_half.pop(0))
-            else:
-                sorted_full.append(first_half.pop(0))
-
-        return sorted_full + first_half + second_half
-
-
-def compare_rows(row1: list[int], row2: list[int]) -> int:
-    r"""
-    A function that compares two rows based on the lowest unique non-zero element
-    index.
-
-    :param row1: binary list
-    :param row2: binary list
-    """
-
-    assert len(row1) == len(row2)
-
-    row1p2 = [(a + b) % 2 for (a, b) in zip(row1, row2)]
-
-    if row1.index(1) == row1p2.index(1):
-        return 0
-    else:
-        return 1
-
-
-# def leading_ones(check_matrix: list[list[int]]) -> dict[int, list[int]]:
-#     r"""
-#     Function taking in a check matrix as a list of binary lists and
-#     outputs a dictionary mapping each index to the list of rows for
-#     which that index is the leading one in the list.
-
-#     :param check_matrix: list of binary lists
-
-#     """
-
-#     leaders = {}
-
-#     for row_index in range(len(check_matrix)):
-#         if check_matrix[row_index].index(1) in leaders:
-#             leaders[check_matrix[row_index].index(1)].append(row_index)
-#         else:
-#             leaders[check_matrix[row_index].index(1)] = [row_index]
-
-#     return leaders
