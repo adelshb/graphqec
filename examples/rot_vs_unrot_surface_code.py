ef07aad2b45fb46536e0830824d1a6e19672bde2
AAA <adel.sohbi@gmail.com>
2025-08-14 20:37:59 -0400

    finalized script

--- examples/rot_vs_unrot_surface_code.py
+++ examples/rot_vs_unrot_surface_code.py
@@ -10,49 +10,75 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import json
 import numpy as np
 import scipy.stats
 import sinter
 import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib.lines import Line2D
+
 from graphqec import RotatedSurfaceCode, UnrotatedSurfaceCode, ThresholdLAB
 
 
-def main(configs: dict[str, int | float], errors: np.ndarray) -> None:
+def main(
+    configs: dict[str, int | float],
+    errors: np.ndarray,
+    max_shots: int,
+    max_errors: int,
+    logic_check: str,
+) -> None:
     r"""
     Compare the performance of Rotated and Unrotated Surface Codes.
+
+    Args:
+        configs: A dictionary containing the configurations for the surface codes.
+        errors: An array of error rates to evaluate.
+        max_shots: The maximum number of shots to simulate.
+        max_errors: The maximum number of errors to consider.
+        logic_check: The logic check to perform.
     """
 
-    # Rotated Surface Code with Z logic
+    # Get stats for the Rotated Surface Code with Z logic
     rot_th_z = ThresholdLAB(
         configurations=configs,
         code=RotatedSurfaceCode,
         error_rates=errors,
         decoder="pymatching",
     )
+    rot_th_z.collect_stats(
+        max_shots=max_shots, max_errors=max_errors, logic_check=logic_check
+    )
 
-    rot_th_z.collect_stats(max_shots=10**7, max_errors=1000, logic_check="Z")
-
-    # Unrotated Surface code with Z logic
+    # Get stats for the Unrotated Surface code with Z logic
     unrot_th_z = ThresholdLAB(
         configurations=configs,
         code=UnrotatedSurfaceCode,
         error_rates=errors,
         decoder="pymatching",
     )
+    unrot_th_z.collect_stats(
+        max_shots=max_shots, max_errors=max_errors, logic_check=logic_check
+    )
 
-    unrot_th_z.collect_stats(max_shots=10**7, max_errors=1000, logic_check="Z")
-
+    # Find the range of values for the fit
     max_n = 0
+    min_n = float("inf")
     for sample in unrot_th_z.samples + rot_th_z.samples:
         max_n = max(max_n, np.sqrt(sample.json_metadata["n"]))
+        min_n = min(min_n, np.sqrt(sample.json_metadata["n"]))
 
+    # Fit each data and plot them on the same plot alongside the fit
+    data = []
+    cmap = matplotlib.colormaps["tab10"]
+    colors = [cmap(i) for i in range(errors.size)]
     fig, ax = plt.subplots(1, 1)
-    for error in errors:
+    for index, error in enumerate(errors):
         if error == 0:
             continue
 
         fit_unrot_z, x_unrot_z, y_unrot_z = fit(unrot_th_z.samples, error)
-        ax.scatter(x_unrot_z, y_unrot_z, marker="^")
+        ax.scatter(x_unrot_z, y_unrot_z, marker="^", color=colors[index], s=50)
         ax.plot(
             [0, max_n],
             [
@@ -61,10 +87,28 @@ def main(configs: dict[str, int | float], errors: np.ndarray) -> None:
             ],
             linestyle="--",
             label=error,
+            color=colors[index],
+            dashes=(2, 2),
+            alpha=0.8,
+        )
+        data.append(
+            {
+                "n": [int(n**2) for n in x_unrot_z],
+                "logical_error": [float(y) for y in y_unrot_z],
+                "type": "Unrotated",
+                "error_rate": float(error),
+                "fit": {
+                    "x": [0, max_n],
+                    "y": [
+                        np.exp(fit_unrot_z.intercept),
+                        np.exp(fit_unrot_z.intercept + fit_unrot_z.slope * max_n),
+                    ],
+                },
+            }
         )
 
         fit_rot_z, x_rot_z, y_rot_z = fit(rot_th_z.samples, error)
-        ax.scatter(x_rot_z, y_rot_z, marker="o")
+        ax.scatter(x_rot_z, y_rot_z, marker="o", color=colors[index], s=50)
         ax.plot(
             [0, max_n],
             [
@@ -73,20 +117,86 @@ def main(configs: dict[str, int | float], errors: np.ndarray) -> None:
             ],
             linestyle="--",
             label=error,
+            color=colors[index],
+            dashes=(2, 2),
+            alpha=0.8,
+        )
+        data.append(
+            {
+                "n": [int(n**2) for n in x_rot_z],
+                "logical_error": [float(y) for y in y_rot_z],
+                "type": "Rotated",
+                "error_rate": float(error),
+                "fit": {
+                    "x": [0, max_n],
+                    "y": [
+                        np.exp(fit_rot_z.intercept),
+                        np.exp(fit_rot_z.intercept + fit_rot_z.slope * max_n),
+                    ],
+                },
+            }
         )
 
+    # Build legend
+    line_handles = [
+        Line2D([0], [0], color=colors[index], lw=2, label=f"{error:.4f}")
+        for index, error in enumerate(errors)
+    ]
+
+    legend_lines = ax.legend(
+        handles=line_handles,
+        title="Physical error rate",
+        loc="upper left",
+        bbox_to_anchor=(1.05, 1.0),
+        borderaxespad=0.0,
+        handlelength=2,
+        frameon=True,
+    )
+    ax.add_artist(legend_lines)
+
+    marker_handles = [
+        Line2D(
+            [0],
+            [0],
+            color="black",
+            marker=m,
+            linestyle="None",
+            markersize=8,
+            label=label,
+        )
+        for m, label in zip(["^", "o"], ["Unrotated", "Rotated"])
+    ]
+
+    lengend_markers = ax.legend(
+        handles=marker_handles,
+        title="Code type",
+        loc="upper left",
+        bbox_to_anchor=(1.05, 0.68),
+        borderaxespad=0.0,
+        frameon=True,
+    )
+
     ax.semilogy()
+    ax.set_xlim(min_n - 2, max_n + 2)
     ax.set_title("")
-    ax.set_xlabel("SQRT(Number of Physical Qubits)")
+    ax.set_xlabel(r"$\sqrt{\mathrm{Number\ of\ Physical\ Qubits}}$")
     ax.set_ylabel("Logical Error Rate per Round")
-    ax.grid(which="major")
-    ax.grid(which="minor")
-    # ax.legend()
-    fig.set_dpi(120)
-    fig.savefig("rot_vs_unrot_surface_code.png")
+    ax.grid(which="major", zorder=0)
+    ax.grid(which="minor", zorder=0)
+    plt.tight_layout()
+    fig.savefig(
+        "rot_vs_unrot_surface_code.png",
+        bbox_inches="tight",
+        bbox_extra_artists=[legend_lines, lengend_markers],
+    )
+
+    with open("rot_vs_unrot_data.json", "w") as f:
+        json.dump(data, f, indent=4)
 
 
-def fit(collected_stats: list[sinter.TaskStats], error):
+def fit(
+    collected_stats: list[sinter.TaskStats], error: float
+) -> tuple[scipy.stats.linregress, list[float], list[float]]:
     r"""
     Compute a line fit for the given collected statistics.
     """
@@ -110,6 +220,15 @@ def fit(collected_stats: list[sinter.TaskStats], error):
 
 if __name__ == "__main__":
 
-    configs = [{"distance": d} for d in [7, 9, 11, 13, 17, 23]]
-    errors = np.linspace(0.001, 0.005, 5)
-    main(configs, errors)
+    max_shots = 100_000
+    max_errors = 100
+    logic_check = "Z"
+    configs = [{"distance": d} for d in [9, 11, 13, 17, 20, 23]]
+    errors = np.linspace(0.001, 0.01, 10)
+    main(
+        configs=configs,
+        errors=errors,
+        max_shots=max_shots,
+        max_errors=max_errors,
+        logic_check=logic_check,
+    )
